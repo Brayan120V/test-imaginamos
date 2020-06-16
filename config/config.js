8fa34ca0ab1c1f60c9cf65d38e823b2b41cdcab0
Brayan Vargas <braflaco@gmail.com>
2020-06-15 22:12:56 -0500

    ping!

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 const isProduction = process.env.NODE_ENV === 'production';
 
-if (process.env.NODE_ENV === 'production') { process.env.DATABASE_URL = process.env.DB_PRODUCTION; } else if (process.env.NODE_ENV === 'test') { process.env.DATABASE_URL = process.env.DB_TEST; } else { process.env.DATABASE_URL = process.env.DB_DEVELOPMENT; }
+if (process.env.NODE_ENV === 'production') { process.env.DATABASE_URL = process.env.DATABASE_URL; } else if (process.env.NODE_ENV === 'test') { process.env.DATABASE_URL = process.env.DB_TEST; } else { process.env.DATABASE_URL = process.env.DB_DEVELOPMENT; }
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
